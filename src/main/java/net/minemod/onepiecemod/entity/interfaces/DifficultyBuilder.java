6749af81a76969a7e21921dd0afec0ed5c87a79e
Darren Johnson <darrenhpjohnson@gmail.com>
2026-09-01 22:49:02 -0500

    Framework for DifficultyBuilder
    Created the framework for the Difficulty Builder class.

---
+++ src/main/java/net/minemod/onepiecemod/entity/interfaces/DifficultyBuilder.java
@@ -0,0 +1,17 @@
+package net.minemod.onepiecemod.entity.interfaces;
+
+/** TODO: Should this class / sub-jm be names something different? */
+public class DifficultyBuilder {
+    // Create Variables with Default values
+
+    // - Bribeable
+
+    // - Pickpocketable
+
+
+    // Builder Methods
+
+    // - BribeDifficulty
+
+    // - PickpocketDifficulty
+}
