d276a6b57e189d1e5e318aaa19ff8e0cee6e0388
Darren Johnson <darrenhpjohnson@gmail.com>
2026-09-08 19:21:45 -0500

    Revert "Added Trades to NPC class"
    This reverts commit a3b2a886db72e8bfbfad3f1b1ad0a2ec6132f404.

--- src/main/java/net/minemod/onepiecemod/entity/npcs/passive/AbstractPassiveNPC.java
+++ src/main/java/net/minemod/onepiecemod/entity/npcs/passive/AbstractPassiveNPC.java
@@ -1,20 +1,10 @@
 package net.minemod.onepiecemod.entity.npcs.passive;
 
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minemod.onepiecemod.entity.interfaces.Bribable;
-import net.minemod.onepiecemod.entity.interfaces.Pickpocketable;
-import net.minemod.onepiecemod.entity.interfaces.Tradable;
 import net.minemod.onepiecemod.entity.npcs.AbstractNPC;
-import net.minemod.onepiecemod.entity.settings.DifficultyBuilder;
-import net.minemod.onepiecemod.entity.trade.Trade;
-
-import java.util.List;
 
 
 /**
@@ -22,23 +12,10 @@ import java.util.List;
  * - Any passive-specific behavior will go here.
  * - (Example: Will never attack, Flees when attacked, etc.)
  */
-public abstract class AbstractPassiveNPC extends AbstractNPC implements Tradable {
+public abstract class AbstractPassiveNPC extends AbstractNPC {
 
-    /** Variables */
-    private final DifficultyBuilder difficulty;
-
-
-    /** Constructors */
-    // This constructor uses Custom Difficulty Settings
-    public AbstractPassiveNPC(EntityType<? extends PathfinderMob> type, Level level, DifficultyBuilder difficulty) {
-        super(type, level);
-        this.difficulty = difficulty;
-    }
-
-    // This constructor uses Default Difficulty Settings
-    public AbstractPassiveNPC(EntityType<? extends PathfinderMob> type, Level level)
-    {
-        this(type, level, new DifficultyBuilder());
+    public AbstractPassiveNPC(EntityType<? extends PathfinderMob> type, Level pLevel) {
+        super(type, pLevel);
     }
 
     /**
@@ -53,22 +30,5 @@ public abstract class AbstractPassiveNPC extends AbstractNPC implements Tradable
 
         // Movement and idle behavior
         this.goalSelector.addGoal(5, new RandomStrollGoal(this, 0.5D));
-    }
-
-    // Trade List - Get Method
-    @Override public List<Trade> getTrades() { return this.difficulty.trades; }
-
-    /**
-     * mobInteract()
-     * - Handles player interactions.
-     * - (Handling events for: Bribe Interaction, Trade Interaction, Pickpocket Interaction, etc
-     */
-    @Override
-    public InteractionResult mobInteract(Player player, InteractionHand hand) {
-        //  Process Trade Interaction
-        if (this.isTradable(player)) {
-            return this.processTrade(this, player, hand);
-        }
-        return super.mobInteract(player, hand);
     }
 }
