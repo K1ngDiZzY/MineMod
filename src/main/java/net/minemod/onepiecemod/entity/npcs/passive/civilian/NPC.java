572f01365dfdbf2dc319c18c52d1d37de225c5c9
Darren Johnson <darrenhpjohnson@gmail.com>
2026-09-08 20:39:27 -0500

    Reapply "Added Trades to NPC class"
    This reverts commit d276a6b57e189d1e5e318aaa19ff8e0cee6e0388.

--- src/main/java/net/minemod/onepiecemod/entity/npcs/passive/civilian/NPC.java
+++ src/main/java/net/minemod/onepiecemod/entity/npcs/passive/civilian/NPC.java
@@ -5,19 +5,28 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minemod.onepiecemod.datagen.ModLootProvider;
 import net.minemod.onepiecemod.entity.npcs.passive.AbstractPassiveNPC;
+import net.minemod.onepiecemod.entity.settings.DifficultyBuilder;
+import net.minemod.onepiecemod.entity.trade.TradeBuilder;
+import net.minemod.onepiecemod.item.ModItems;
 
 /**
- * This class represents a "Test-Dummy" NPC that is used for testing purposes. Does not move, and has High HP.
+ * This class represents a "Test-Dummy" NPC that is used for testing purposes. Does not run, and has High HP.
  * - (This class is placed in the "npcs.passive.civilian" package, because Civilian NPCs are coming soon.)
  */
 public class NPC extends AbstractPassiveNPC {
 
-    public NPC(EntityType<? extends PathfinderMob> type, Level pLevel) {
-        super(type, pLevel);
+    public NPC(EntityType<? extends PathfinderMob> type, Level level) {
+        super(type, level, new DifficultyBuilder()
+                .trades(TradeBuilder.create()
+                        .addTrade(ModItems.BERRY.get(), 64, Items.BARRIER, 1)
+                        .addTrade(ModItems.BERRY.get(), 64, ModItems.BERRY.get(), 64, Items.COMMAND_BLOCK, 1)
+                )
+        );
     }
 
     @Override
